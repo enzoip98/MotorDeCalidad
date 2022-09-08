8d184d2dc6035d588c52acadab45730f81412287
Enzo Ipanaque <enzo.ipanaque.contractor@bbva.com>
2022-09-08 10:41:41 -0500

    version_0.1

--- motordecalidad/functions.py
+++ motordecalidad/functions.py
@@ -1,27 +1,19 @@
 from typing import List
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import col,when,lit
-from pyspark.sql.types import StringType
+from pyspark.sql.types import StringType, IntegerType
 
 ##Constants
-NullString = "_null"
-DuplicatedString = "_duplicated"
-One = 1
-FalseValue = False
 NullRuleCode = "101"
 DuplicatedRuleCode = "102"
 
-def validateNull(object:DataFrame,field: StringType): 
-    nullColumnName = field + NullString
-    nullDf = object.select(field).withColumn(nullColumnName,when(col(field).isNull(),One))
-    nullCount = nullDf.filter(col(nullColumnName)==lit(FalseValue)).count()
-    registersAmount = object.count()
+def validateNull(object:DataFrame,field: StringType,registersAmount: IntegerType): 
+    nullCount = object.select(field).filter(col(field).isNull()).count()
     ratio = nullCount/ registersAmount
     return [NullRuleCode,ratio,nullCount]
 
-def validateDuplicates(object:DataFrame,fields:List):
+def validateDuplicates(object:DataFrame,fields:List,registersAmount: IntegerType):
     uniqueRegistersAmount = object.select(fields).dropDuplicates().count()
-    registersAmount = object.count()
     nonUniqueRegistersAmount = registersAmount - uniqueRegistersAmount
     ratio = nonUniqueRegistersAmount / registersAmount
     return [DuplicatedRuleCode,ratio,nonUniqueRegistersAmount]
