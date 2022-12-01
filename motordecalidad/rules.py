d879ae6331516fa9a63dc98c13824448d408de3f
Enzo_Ipanaque <enzo.fernando.ipana1@managementsolutions.com>
2022-12-01 10:16:21 -0500

    correcion

--- motordecalidad/rules.py
+++ motordecalidad/rules.py
@@ -405,12 +405,12 @@ def chooseOper(col,op:str):
     if op=='<':
         return operator.ge
 
-def measuresCentralTendency(object:DataFrame, columnNames):
-    
+def measuresCentralTendency(object:DataFrame, columnNames, spark):
     object=object.select(columnNames)
     modes=["Mode"]
     for column in columnNames:
         modes.append(object.groupby(column).count().orderBy("count", ascending=False).first()[0])
+        res=object.summary('mean','25%','50%','75%')
         res=object.summary('mean','25%','50%','75%')
 
     mode = spark.createDataFrame([modes], columnNames)
